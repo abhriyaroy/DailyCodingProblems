1834058d8da42dafcdd724acc5b5e4b28a614e17
royabh <roy.abhriya@gmail.com>
2018-10-04 09:24:19 +0530

    Solve day2

---
+++ day2.java
@@ -0,0 +1,39 @@
+package dcm;
+
+import java.util.Arrays;
+
+public class day2 {
+
+    /* This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?*/
+
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(findArray(arr)));
+    }
+
+    static int[] findArray(int arr[]) {
+        int product = 1;
+        for (int a : arr) {
+            product *= a;
+        }
+        int count;
+        int resultantArray[] = new int[arr.length];
+        for (int a = 0; a < arr.length; a++) {
+            count = 0;
+            int num = product;
+            while (num > 0) {
+                num -= arr[a];
+                count++;
+            }
+            resultantArray[a] = count;
+        }
+        return resultantArray;
+    }
+
+}
--- day4.java
+++ day4.java
@@ -5,21 +5,28 @@ import java.util.TreeMap;
 
 public class day4 {
 
+
+    /*This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.*/
+
     public static void main(String args[]) {
-        int a[] = {1,2,0};
+        int a[] = {1, 2, 0};
         HashMap<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < a.length; i++) {
             hashMap.put(a[i], i);
         }
-       int i=1;
-        while (i< Integer.MAX_VALUE){
-            if (!hashMap.containsKey(i)){
+        int i = 1;
+        while (i < Integer.MAX_VALUE) {
+            if (!hashMap.containsKey(i)) {
                 System.out.println(i);
                 break;
             }
             i++;
         }
-
-        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
     }
 }
