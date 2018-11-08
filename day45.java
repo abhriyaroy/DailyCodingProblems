9e5a6c9ab263dc9eb33184397313c75da533bd90
royabh <roy.abhriya@gmail.com>
2018-11-08 16:43:20 +0530

    Solve day 45 question added

--- day45.java
+++ day45.java
@@ -5,6 +5,10 @@ import java.util.Random;
 
 public class day45 {
 
+    /*This problem was asked by Two Sigma.
+
+Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform probability, implement a function rand7() that returns an integer from 1 to 7 (inclusive).*/
+
     public static void main(String args[]) {
         System.out.println(rand7());
     }
